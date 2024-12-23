9226fd9043b4c9b833cc416df11bbae3ed70c757
Wayne <Euwayne2010@gmail.com>
2024-12-23 22:11:31 +0100

    Updated Filter Functionality

--- rust/src/api/data_handler.rs
+++ rust/src/api/data_handler.rs
@@ -20,9 +20,9 @@ pub struct DataHandler {
     current_time: DateTime<Utc>,
     day: u8,
     
-    // byte_array: Vec<Vec<u16>>,
-    pub data_list: Vec<u16>,
-    pub error: bool,
+    filter_matrix: Vec<Vec<u16>>,
+    data_list: Vec<u16>,
+    pub error: bool
 }
 
 impl DataHandler {
@@ -31,7 +31,7 @@ impl DataHandler {
     pub fn new(stream_sinks: Vec<StreamSink<i32>>, num_channels: u8, dir: String, filename: String) -> Self {
         // Create empty arrays to hold the returned data and byte array
         let data_list: Vec<u16> = Vec::with_capacity(num_channels as usize);
-        let byte_array: Vec<Vec<u16>> = Vec::with_capacity(num_channels as usize);
+        let filter_matrix: Vec<Vec<u16>> = vec![Vec::new(); num_channels as usize];
 
         // Set this DataHandler for Day 1
         let day: u8 = 1;
@@ -42,13 +42,14 @@ impl DataHandler {
         // Creates CSV File
         let (writer, error, current_time) = Self::create_file(&csv_path, day, num_channels);
 
-        DataHandler { stream_sinks, csv_path, writer, num_channels, current_time, day, data_list, error }
+        DataHandler { stream_sinks, csv_path, writer, num_channels, current_time, day, filter_matrix, data_list, error }
     }
 
 
     /*  
         This Function Takes in the pathway to the CSV File, the current day and the number of Channels being monitored
         - Creates a File Specified by the csv_path
+            - panics if the filename given aready Exists!
         - Adds a Header in the CSV File with the Date and the Number of Channels.
         - Returns a Writer Object for the Created File, A boolean value indicating if an Error has occured, and the Current Date and Time the file was created.
     */
@@ -141,21 +142,21 @@ impl DataHandler {
                 // Combine the Byte Values (With Shift and Or Operation)
                 let processed_datapoint: u16 = ((byte_one as u16) << 8) | (byte_two as u16);
 
-                // Everytime a Datapoint is Processed
-                self.data_list.push(processed_datapoint);                   // Add into data_list
+                // Everytime a Datapoint is Processed, Add it into data_list
+                self.data_list.push(processed_datapoint);
 
-                // Need Change: Filter First and then Push to Flutter
-                // self.stream_sinks[current_channel].add(processed_datapoint) // Send Datapoint to Flutter 
-                //   .expect("Error: Failed to add value to Stream");
-
-                current_channel += 1;
+                current_channel += 1; // Increment Current Channel Count
 
             }
 
             // Once Each Channel has a Processed Data Point
             if current_channel == self.num_channels {
-                // Save (Write) it into the CSV File
+                // Clone data_list
+                // Reason: Throws an error if you borrow two different mutable references of self
                 let cloned_data = self.data_list.clone();
+
+                // Save (Write) it into the CSV File
+                self.filter(&cloned_data);
                 self.save_data_csv(&cloned_data).await;
 
                 self.data_list.clear(); // Clear the data_list after Saving
@@ -166,38 +167,73 @@ impl DataHandler {
 
     }
 
-    // Previous process Function
+    
     /*
-    pub async fn process(&mut self, bytes: &[u8]) {
-        let mut byte_one: u8 = 0x00;
-        // Loop Through the Given Byte Array
-        for (i, b) in bytes.iter().enumerate() {
-            // For Debugging
-            //println!("{} {}", i, *b);
-
-            if i % 2 == 0 {
-                // Assign the Even bytes to byte_one
-                byte_one = *b;
-            } 
-            else if i % 2 != 0 {
-                // Assign the Odd bytes to byte_two
-                let byte_two = *b;
-                
-                // For Debugging
-                // println!("{} {}", byte_one, b);
-
-                // Combine the Byte Values (With Shift and Or Operation) and push to data_list
-                self.data_list.push( ((byte_one as u16) << 8) | (byte_two as u16) );
-            }
-
+        This Function takes in data_list. data_list contains a processed Data Point for each channel
+        - Adds each Data Point to its corresponding Channel Vector in the filter_matrix
+        - When "each channel" has 10 elemets, sum them up
+        - Send the sum of each channel to Flutter via its corresponding StreamSink
+        - Clear filter_matrix and reallocate some memory for it
+        Note: The function assumes EACH CHANNEL has 10 elements if the FIRST channel has 10 elements.
+        This assumption is based on the fact that data_list is only sent when it has the a Data Point
+        for each channel.
+    */
+    async fn filter(&mut self, data_list: &Vec<u16>) -> Result<()> {
+        // For each Data Point, add the Data Point to its corresponding Channel Vector in the filter_matrix
+        for (channel, value) in data_list.iter().enumerate() {
+            self.filter_matrix[channel].push(*value);
         }
 
-        self.filter();
+        // Once "each channel" has 10 Data Points
+        if self.filter_matrix[0].len() == 10 {
+            for (channel, points) in self.filter_matrix.iter().enumerate() {
 
-        self.save_data_csv().await;
+                // Take the sum of all the Data Points
+                let sum: i32 = points.iter().map(|&val| val as i32).sum();
+
+                // Send sum to Flutter via the corresponding StreamSink
+                self.stream_sinks[channel].add(sum).expect("Error streaming data")
+                /*
+                    Note: Once in flutter the sum will be divided by 10 to get the average.
+                    This is done since stream sinks can only send i32 values.
+                 */
+            }
+
+            self.filter_matrix.clear(); // Clear filter_matrix
+            // Reallocate some memory for the new filter_matrix
+            self.filter_matrix = vec![Vec::new(); self.num_channels as usize];
+        }
+
+        Ok(())
+    }
+
+
+    // Previous filter Function
+    /*
+    async fn filter(&mut self) -> Result<()> {
+        // Enumerate over data_list, this is done to get the correct index of the byte_array and stream sink for the current channel
+        self.data_list.iter().enumerate().map(|(ind, &val)| {
+            let byte_array: &mut Vec<u16> = &mut self.byte_array[ind]; // byte array for the current channel
+
+            byte_array.push(val); // Add the new value for the channel
+
+            // If the byte array is 10 elements long, take the sum of all the data points and send it back to flutter
+            // Once in flutter the sum will be divided by 10 to get the average. This is done since stream sinks can only send
+            // i32 values.
+            if byte_array.len() == 10 {
+                let sum: i32 = byte_array.iter().map(|&val| val as i32).sum::<i32>(); // Sum elements of the byte array
+
+                // Calling the add method on the stream sink for this channel sends the data to flutter
+                self.stream_sinks[ind].add(sum).expect("Error streaming data"); //Implement better error handling
+
+                byte_array.clear(); // Clear the byte array
+            }
+        });
+
+        Ok(())
     }
     */
-
+    
 
     /* 
         This Function Takes in data, a list of u16 Integers (One for each Channel) which corresponds to the Channel Value
@@ -236,32 +272,6 @@ impl DataHandler {
         }
 
     } 
-
-    // Filter Function
-    /*
-    async fn filter(&mut self) -> Result<()> {
-        // Enumerate over data_list, this is done to get the correct index of the byte_array and stream sink for the current channel
-        self.data_list.iter().enumerate().map(|(ind, &val)| {
-            let byte_array: &mut Vec<u16> = &mut self.byte_array[ind]; // byte array for the current channel
-
-            byte_array.push(val); // Add the new value for the channel
-
-            // If the byte array is 10 elements long, take the sum of all the data points and send it back to flutter
-            // Once in flutter the sum will be divided by 10 to get the average. This is done since stream sinks can only send
-            // i32 values.
-            if byte_array.len() == 10 {
-                let sum: i32 = byte_array.iter().map(|&val| val as i32).sum::<i32>(); // Sum elements of the byte array
-
-                // Calling the add method on the stream sink for this channel sends the data to flutter
-                self.stream_sinks[ind].add(sum).expect("Error streaming data"); //Implement better error handling
-
-                byte_array.clear(); // Clear the byte array
-            }
-        });
-
-        Ok(())
-    }
-    */
 
 
     /*
