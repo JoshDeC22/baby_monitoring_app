9322b3381c1fb9ca4292477b772398eb406f7719
Wayne <Euwayne2010@gmail.com>
2025-01-12 14:37:37 +0000

    Aligning Local Files with Josh

--- lib/screens/live_plot_page.dart
+++ lib/screens/live_plot_page.dart
@@ -8,11 +8,19 @@ class LivePlotPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    DateTime dummyTime = DateTime.parse('2025-01-01 11:00:00Z');
     final List<ChartData> glucoseData = [
-      ChartData(DateTime.now(), 4),
-      ChartData(DateTime.now().add(const Duration(minutes: 1)), 5),
-      ChartData(DateTime.now().add(const Duration(minutes: 2)), 4),
-      ChartData(DateTime.now().add(const Duration(minutes: 3)), 5),
+      ChartData(dummyTime, 4),
+      ChartData(dummyTime.add(const Duration(minutes: 1)), 5),
+      ChartData(dummyTime.add(const Duration(minutes: 2)), 4),
+      ChartData(dummyTime.add(const Duration(minutes: 3)), 5),
+      ChartData(dummyTime.add(const Duration(minutes: 4)), 2),
+      ChartData(dummyTime.add(const Duration(minutes: 5)), 1),
+      ChartData(dummyTime.add(const Duration(minutes: 6)), 3),
+      ChartData(dummyTime.add(const Duration(minutes: 7)), 2),
+      ChartData(dummyTime.add(const Duration(minutes: 8)), 4),
+      ChartData(dummyTime.add(const Duration(minutes: 9)), 6),
+      ChartData(dummyTime.add(const Duration(hours: 1)), 3),
     ];
     final List<ChartData> lactateData = [
       ChartData(DateTime.now(), 1),
@@ -23,8 +31,18 @@ class LivePlotPage extends StatelessWidget {
       appBar: AppBar(title: const Text('Live Data Plot')),
       body: Column(
         children: [
-          Expanded(child: GraphWidget(number: 1, paramName: 'glucose', data: glucoseData, lineColor: Colors.green)),
-          Expanded(child: GraphWidget(number: 2, paramName: 'lactate', data: lactateData, lineColor: Colors.red)),
+          Expanded(
+              child: GraphWidget(
+                  number: 1,
+                  paramName: 'glucose',
+                  data: glucoseData,
+                  lineColor: Colors.green)),
+          Expanded(
+              child: GraphWidget(
+                  number: 2,
+                  paramName: 'lactate',
+                  data: lactateData,
+                  lineColor: Colors.red)),
         ],
       ),
     );
--- lib/widgets/graph.dart
+++ lib/widgets/graph.dart
@@ -2,13 +2,11 @@
 import 'package:baby_monitoring_app/widgets/data_model.dart';
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
-import 'comment_popup.dart';
 
-// This class is the base unit for all the plots. It can be used for both live plotting and static plotting
 class GraphWidget extends StatefulWidget {
-  final int number; // The plot number
-  final List<ChartData> data; // data used for the plot
-  final String paramName; // 
+  final int number;
+  final List<ChartData> data;
+  final String paramName;
   final Color lineColor;
 
   const GraphWidget({
@@ -19,15 +17,14 @@ class GraphWidget extends StatefulWidget {
     required this.lineColor,
   });
 
-    @override
-    GraphWidgetState createState() => GraphWidgetState();
+  @override
+  GraphWidgetState createState() => GraphWidgetState();
 }
 
 class GraphWidgetState extends State<GraphWidget> {
   late ZoomPanBehavior _zoomPanBehavior;
   late TooltipBehavior _tooltipBehavior;
   late DateTimeAxis _xAxis;
-  final ValueNotifier<List<CartesianChartAnnotation>> annotations = ValueNotifier<List<CartesianChartAnnotation>>([]);
 
   @override
   void initState() {
@@ -39,7 +36,7 @@ class GraphWidgetState extends State<GraphWidget> {
     // Initialize ZoomPanBehavior
     _zoomPanBehavior = ZoomPanBehavior(
       enablePinching: true, // Enable pinching to zoom
-      //enableDoubleTapZooming: true, // Enable double-tap zooming
+      enableDoubleTapZooming: true, // Enable double-tap zooming
       enablePanning: true, // Enable scrolling
       enableSelectionZooming: true,
       zoomMode: ZoomMode.x, // Allow horizontal scrolling only
@@ -48,9 +45,11 @@ class GraphWidgetState extends State<GraphWidget> {
     _xAxis = DateTimeAxis(
       title: const AxisTitle(text: 'Time of Day'),
       enableAutoIntervalOnZooming: true,
-      edgeLabelPlacement: EdgeLabelPlacement.shift, //prevents time labels at edges from being cut off
-      initialVisibleMinimum: widget.data.last.time.subtract(const Duration(minutes: 5)), // Intially show post recent 5 minutes of data
-      initialVisibleMaximum: widget.data.last.time, 
+      edgeLabelPlacement: EdgeLabelPlacement
+          .shift, //prevents time labels at edges from being cut off
+      initialVisibleMinimum: widget.data.last.time.subtract(
+          const Duration(minutes: 5)), // Intially show post recent 5 minutes of data
+      initialVisibleMaximum: widget.data.last.time,
     );
   }
 
@@ -73,54 +72,87 @@ class GraphWidgetState extends State<GraphWidget> {
                       data: widget.data,
                       paramName: widget.paramName,
                       lineColor: widget.lineColor,
-                      annotations: annotations,
                     ),
                   ),
                 );
               },
             ),
           ),
-          ValueListenableBuilder<List<CartesianChartAnnotation>>(
-            valueListenable: annotations, 
-            builder: (BuildContext context, List<CartesianChartAnnotation> annotationList, Widget? child) {
-              return SizedBox(
-                height: 200,
-                child: GestureDetector(
-                  onDoubleTapDown: (details) {
-                    final RenderBox box = context.findRenderObject() as RenderBox;
+          SizedBox(
+            height: 200,
+            child: GestureDetector(
+              onDoubleTapDown: (details) {
+                final RenderBox box = context.findRenderObject() as RenderBox;
 
-                    final widgetWidth = box.size.width;
+                final widgetWidth = box.size.width;
 
-                    final x = details.localPosition.dx;
+                final x = details.localPosition.dx;
 
-                    final dataPoint = _getDataPointFromX(x, widgetWidth, _xAxis, widget.data);
+                final dataPoint = _getDataPointFromX(x, widgetWidth);
 
-                    _showCommentPopup(context, dataPoint, annotations);
-                  },
-                  child: SfCartesianChart(
-                    legend: const Legend(isVisible: true),
-                    tooltipBehavior: _tooltipBehavior, //enabling tooltips
-                    zoomPanBehavior:
-                        _zoomPanBehavior, //enabling zooming and panning
-                    primaryXAxis: _xAxis,
-                    series: <LineSeries<ChartData, DateTime>>[
-                      LineSeries<ChartData, DateTime>(
-                        dataSource: widget.data,
-                        color: widget.lineColor,
-                        enableTooltip: true,
-                        xValueMapper: (ChartData data, _) => data.time,
-                        yValueMapper: (ChartData data, _) => data.bitVal,
-                      ),
-                    ],
-                    annotations: annotations.value,
+                //_showCommentPopup(context, dataPoint);
+              },
+              child: SfCartesianChart(
+                legend: const Legend(isVisible: true),
+                tooltipBehavior: _tooltipBehavior, //enabling tooltips
+                zoomPanBehavior:
+                    _zoomPanBehavior, //enabling zooming and panning
+                primaryXAxis: _xAxis,
+                series: <LineSeries<ChartData, DateTime>>[
+                  LineSeries<ChartData, DateTime>(
+                    dataSource: widget.data,
+                    color: widget.lineColor,
+                    enableTooltip: true,
+                    xValueMapper: (ChartData data, _) => data.time,
+                    yValueMapper: (ChartData data, _) => data.bitVal,
                   ),
-                ),
-              );
-            },
+                ],
+              ),
+            ),
           ),
         ],
       ),
     );
+  }
+
+  // void _showCommentPopup(BuildContext context, ChartData dataPoint) {
+  //   showDialog(
+  //     context: context,
+  //     builder: (BuildContext context) {
+  //       return CommentPopup(time: dataPoint.time, bitVal: dataPoint.bitVal);
+  //     },
+  //   );
+  // }
+
+  ChartData _getDataPointFromX(double x, double width) {
+    final xMin = _xAxis.minimum!; // Error handle
+    final xMax = _xAxis.maximum!; // Error handle
+
+    final xMinIndex = _getIndexFromTime(widget.data, xMin.toString());
+    final xMaxIndex = _getIndexFromTime(widget.data, xMax.toString());
+
+    final numPoints = xMaxIndex - xMinIndex + 1;
+
+    final xProportion = x / width;
+
+    final index = xMinIndex + (numPoints * xProportion).floor();
+
+    return widget.data[index];
+  }
+
+  int _getIndexFromTime(List<ChartData> data, String time) {
+    int index = 0;
+
+    for (int i = 0; i < data.length; i++) {
+      String dataPointTime = data[i].time.toString();
+
+      if (dataPointTime == time) {
+        index = i;
+        break;
+      }
+    }
+
+    return index;
   }
 }
 
@@ -129,24 +161,21 @@ class ExpandedGraphPage extends StatefulWidget {
   final List<ChartData> data;
   final String paramName;
   final Color lineColor;
-  final ValueNotifier<List<CartesianChartAnnotation>> annotations;
 
   const ExpandedGraphPage({
     super.key,
     required this.data,
     required this.paramName,
     required this.lineColor,
-    required this.annotations,
   });
 
   @override
-  ExpandedGraphPageState createState() => ExpandedGraphPageState();
+  _ExpandedGraphPageState createState() => _ExpandedGraphPageState();
 }
 
-class ExpandedGraphPageState extends State<ExpandedGraphPage> {
+class _ExpandedGraphPageState extends State<ExpandedGraphPage> {
   late ZoomPanBehavior _zoomPanBehavior;
   late TooltipBehavior _tooltipBehavior;
-  late DateTimeAxis _xAxis;
 
   @override
   void initState() {
@@ -158,18 +187,10 @@ class ExpandedGraphPageState extends State<ExpandedGraphPage> {
     // Initialize ZoomPanBehavior
     _zoomPanBehavior = ZoomPanBehavior(
       enablePinching: true, // Enable pinching to zoom
-      //enableDoubleTapZooming: true, // Enable double-tap zooming
+      enableDoubleTapZooming: true, // Enable double-tap zooming
       enablePanning: true, // Enable scrolling
       enableSelectionZooming: true,
       zoomMode: ZoomMode.x, // Allow horizontal scrolling only
-    );
-
-    _xAxis = DateTimeAxis(
-      title: const AxisTitle(text: 'Time of Day'),
-      enableAutoIntervalOnZooming: true,
-      edgeLabelPlacement: EdgeLabelPlacement.shift, //prevents time labels at edges from being cut off
-      initialVisibleMinimum: widget.data.last.time.subtract(const Duration(minutes: 5)), // Intially show post recent 5 minutes of data
-      initialVisibleMaximum: widget.data.last.time, 
     );
   }
 
@@ -178,84 +199,28 @@ class ExpandedGraphPageState extends State<ExpandedGraphPage> {
     return Scaffold(
       appBar: AppBar(title: Text('Expanded Graph - ${widget.paramName}')),
       body: Center(
-        child: ValueListenableBuilder<List<CartesianChartAnnotation>>(
-          valueListenable: widget.annotations, 
-          builder: (BuildContext context, List<CartesianChartAnnotation> annotationList, Widget? child) {
-            
-
-            return GestureDetector(
-              onDoubleTapDown: (details) {
-                final RenderBox box = context.findRenderObject() as RenderBox;
-
-                final widgetWidth = box.size.width;
-
-                final x = details.localPosition.dx;
-
-                final dataPoint = _getDataPointFromX(x, widgetWidth, _xAxis, widget.data);
-
-               _showCommentPopup(context, dataPoint, widget.annotations);
-              },
-              child: SfCartesianChart(
-                legend: const Legend(isVisible: true),
-                tooltipBehavior: _tooltipBehavior, //enabling tooltips
-                zoomPanBehavior:_zoomPanBehavior, //enabling zooming and panning
-                primaryXAxis: _xAxis,
-                series: <LineSeries<ChartData, DateTime>>[
-                  LineSeries<ChartData, DateTime>(
-                    dataSource: widget.data,
-                    color: widget.lineColor,
-                    enableTooltip: true,
-                    xValueMapper: (ChartData data, _) => data.time,
-                    yValueMapper: (ChartData data, _) => data.bitVal,
-                    animationDuration: 0,
-                  ),
-                ],
-                annotations: widget.annotations.value,
-              ),
-            );
-          },
+        child: SfCartesianChart(
+          tooltipBehavior: _tooltipBehavior, //enabling tooltips
+          zoomPanBehavior: _zoomPanBehavior, //enabling zooming and panning
+          primaryXAxis: DateTimeAxis(
+            title: const AxisTitle(text: 'Time of Day'),
+            enableAutoIntervalOnZooming: true,
+            edgeLabelPlacement: EdgeLabelPlacement
+                .shift, //prevents time labels at edges from being cut off
+            initialVisibleMinimum: widget.data.last.time.subtract(Duration(
+                minutes: 5)), // Intially show post recent 5 minutes of data
+            initialVisibleMaximum: widget.data.last.time,
+          ),
+          series: <LineSeries<ChartData, DateTime>>[
+            LineSeries<ChartData, DateTime>(
+              dataSource: widget.data,
+              color: widget.lineColor,
+              xValueMapper: (ChartData data, _) => data.time,
+              yValueMapper: (ChartData data, _) => data.bitVal,
+            ),
+          ],
         ),
       ),
     );
   }
-}
-
-ChartData _getDataPointFromX(double x, double width, DateTimeAxis axis, List<ChartData> data) {
-  final xMin = axis.initialVisibleMinimum!; // Error handle
-  final xMax = axis.initialVisibleMaximum!; // Error handle
-
-  final xMinIndex = _getIndexFromTime(data, xMin.toString());
-  final xMaxIndex = _getIndexFromTime(data, xMax.toString());
-
-  final numPoints = xMaxIndex - xMinIndex + 1;
-
-  final xProportion = x / width;
-
-  final index = xMinIndex + (numPoints * xProportion).floor();
-
-  return data[index];
-}
-
-int _getIndexFromTime(List<ChartData> data, String time) {
-  int index = 0;
-
-  for (int i = 0; i < data.length; i++) {
-    String dataPointTime = data[i].time.toString();
-
-    if (dataPointTime == time) {
-      index = i;
-      break;
-    }
-  }
-
-  return index;
-}
-
-void _showCommentPopup(BuildContext context, ChartData dataPoint, ValueNotifier<List<CartesianChartAnnotation>> annotations) {
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return CommentPopup(time: dataPoint.time, bitVal: dataPoint.bitVal, annotations: annotations);
-    },
-  );
 }
