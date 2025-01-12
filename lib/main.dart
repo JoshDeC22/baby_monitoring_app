643e54b008c881b5856ccbe8977f54ebabcce819
JoshDeC22 <joshua.decoteau22@imperial.ac.uk>
2025-01-12 21:08:34 +0000

    Minor edits to backend connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
+import 'package:baby_monitoring_app/screens/first_screen.dart';
 import 'package:baby_monitoring_app/utils/app_state_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:baby_monitoring_app/src/rust/frb_generated.dart';
 import 'package:provider/provider.dart';
-//import 'package:baby_monitoring_app/src/rust/api/data_handler.dart';
-import 'screens/bluetooth_device_list/bluetooth_le_device_list.dart';
 
 Future<void> main() async {
   await RustLib.init();
@@ -18,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: BluetoothLEDevicePage()
+      home: HomeScreen(),
     );
   }
 }
