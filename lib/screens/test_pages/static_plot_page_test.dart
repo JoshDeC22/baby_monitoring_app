a96649b839ede94030f43d6e703a0ef08ff462a1
maellaptop <ms4822@ic.ac.uk>
2025-01-13 23:14:20 +0000

    added import to fix comment issue on statictest

--- lib/screens/test_pages/static_plot_page_test.dart
+++ lib/screens/test_pages/static_plot_page_test.dart
@@ -1,19 +1,15 @@
-// live_plot_page.dart
-import 'dart:math';
-
-import 'package:baby_monitoring_app/utils/data_model.dart';
 import 'package:flutter/material.dart';
 import '../../widgets/graph.dart';
-import 'package:baby_monitoring_app/utils/colors.dart';
-import 'package:baby_monitoring_app/screens/static_plot_page.dart';
+import 'package:baby_monitoring_app/utils/data_model.dart';
+import 'package:baby_monitoring_app/utils/comment_data.dart'; // Import CommentData
 
 class StaticPlotPageTest extends StatelessWidget {
   const StaticPlotPageTest({super.key});
 
-
   @override
   Widget build(BuildContext context) {
-    DateTime dummyTime = DateTime.parse('2025-01-01 11:00:00'); // Year/Month/Day
+    DateTime dummyTime = DateTime.parse('2025-01-01 11:00:00'); 
+
     final List<ChartData> dummyData = [
       ChartData(dummyTime, 4),
       ChartData(dummyTime.add(const Duration(minutes: 1)), 5),
@@ -25,71 +21,43 @@ class StaticPlotPageTest extends StatelessWidget {
       ChartData(dummyTime.add(const Duration(minutes: 7)), 2),
       ChartData(dummyTime.add(const Duration(minutes: 8)), 4),
       ChartData(dummyTime.add(const Duration(minutes: 9)), 6),
+      ChartData(dummyTime.add(const Duration(hours: 1)), 3),
     ];
-    final data = [dummyData, dummyData];
+
+    final data = [dummyData, dummyData]; 
     final names = ["Glucose", "Lactate"];
+    final List<CommentData> comments = []; // Initialize comments
 
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(60.0), 
-        child: Container(
-          decoration: BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.5), 
-                spreadRadius: 1, 
-                blurRadius: 10, 
-                offset: Offset(0, 3), 
-              ),
-            ],
-          ),
-          child: AppBar(
-            title: const Text(
-              'Static Blood Parameter Monitoring',
-              style: TextStyle(color: Colors.white, fontSize: 30.0, fontWeight: FontWeight.bold),
-            ),
-            backgroundColor: AppColors.darkBlue, 
-            centerTitle: true,
-            actions: <Widget>[
-              TextButton(
-                onPressed: () {
-                  // TODO: navigate back to home
-                },
-                style: TextButton.styleFrom(
-                  backgroundColor: AppColors.palePink, 
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0), 
-                    side: BorderSide(color: AppColors.darkPink, width: 1.5), 
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'Static Blood Parameter Plot (Dummy Data)',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              for (int i = 0; i < data.length; i++)
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: GraphWidget(
+                    number: i + 1,
+                    data: data[i],
+                    paramName: names[i],
+                    lineColor: i == 0 ? Colors.red : Colors.green,
+                    plotType: 's',
+                    commentData: comments, // Pass comments
                   ),
-                ),
-                child: const Text('Go Home', style: TextStyle(color: Colors.black, fontSize: 15.0)),
-              ),
-              IconButton(
-                icon: const Icon(Icons.arrow_drop_down, color: Colors.white, size: 50),
-                onPressed: () => {
-                  // TODO: navigate back to home
-                }
-              ),
+                )
             ],
           ),
         ),
-      ),
-      body: Column(
-        children: [
-          for (int i = 0; i < data.length; i++)
-            if (i == 0) 
-              Expanded(
-                child: GraphWidget(number: 1, data: data[i], paramName: names[i], lineColor: Colors.red, plotType: 's', commentData: comments),
-              )
-            else if (i == 1)
-              Expanded(
-                child: GraphWidget(number: 1, data: data[i], paramName: names[i], lineColor: Colors.green, plotType: 's', commentData: comments),
-              )
-            else
-              Expanded(
-                child: GraphWidget(number: i + 1, data: data[i], paramName: names[i], lineColor: generateRandomColor(), plotType: 's', commentData: comments),
-              )
-        ],
       ),
     );
   }
