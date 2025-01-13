15de9bfe3e13e2b4d3f0948cec665667ad1d9d4d
JoshDeC22 <joshua.decoteau22@imperial.ac.uk>
2025-01-13 15:58:34 +0000

    Merged the last of the files

--- lib/screens/bluetooth_page.dart
+++ lib/screens/bluetooth_page.dart
@@ -1,5 +1,7 @@
-// bluetooth_page.dart
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
+import 'package:baby_monitoring_app/utils/colors.dart'; // Ensure colors.dart is correctly imported
 
 class BluetoothDevicePage extends StatelessWidget {
   const BluetoothDevicePage({super.key});
@@ -7,8 +9,57 @@ class BluetoothDevicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Bluetooth Device Selection')),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(80.0), //AppBar height
+        child: Container(
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.5), 
+                spreadRadius: 1, 
+                blurRadius: 10, 
+                offset: Offset(0, 3),
+              ),
+            ],
+          ),
+          child: AppBar(
+            title: const Text(
+              'Bluetooth Device Selection',
+              style: TextStyle(color: Colors.white, fontSize: 35.0, fontWeight: FontWeight.bold),
+            ),
+            backgroundColor: AppColors.darkBlue, 
+            centerTitle: true,
+            actions: <Widget>[
+              IconButton(
+                icon: const Icon(Icons.arrow_drop_down, color: Colors.white, size: 50), 
+                onPressed: () => _showPopupMenu(context),
+              ),
+            ],
+          ),
+        ),
+      ),
       body: const BluetoothDeviceList(),
+    );
+  }
+
+  void _showPopupMenu(BuildContext context) async {
+    await showMenu<String>(
+      context: context,
+      position: RelativeRect.fromLTRB(200.0, 80.0, 0.0, 0.0), //IconButton position
+      items: [
+        PopupMenuItem<String>(
+          value: 'past_recordings',
+          onTap: () {
+            Navigator.pushNamed(context, '/pastRecordings'); // Navigation to past recordings
+          },
+          child: 
+          const Text('Go to past recordings',
+          textAlign: TextAlign.center, 
+          style: TextStyle(fontSize: 18), 
+          ),
+        ),
+      ],
+      elevation: 8.0,
     );
   }
 }
@@ -21,32 +72,54 @@ class BluetoothDeviceList extends StatefulWidget {
 }
 
 class _BluetoothDeviceListState extends State<BluetoothDeviceList> {
-  List<String> availableDevices = ["Device A", "Device B", "Device C"];
-
-  Future<void> scanForDevices() async {
-    setState(() {
-      availableDevices = ["Device X", "Device Y", "Device Z"];
-    });
-  }
+  List<String> availableDevices = ['Device A', 'Device B', 'Device C'];
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          child: ListView.builder(
-            itemCount: availableDevices.length,
-            itemBuilder: (context, index) {
-              String deviceName = availableDevices[index];
-              return ListTile(
-                title: Text(deviceName),
-                onTap: () => Navigator.pushNamed(context, '/livePlot'),
-              );
-            },
-          ),
+    return Padding(
+      padding: const EdgeInsets.only(top: 20), 
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start, 
+          children: <Widget>[
+            const Padding(
+              padding: EdgeInsets.all(30.0),
+              child: Text(
+                'Select a device for connection',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            for (String deviceName in availableDevices)
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: SizedBox(
+                  width: 350,  // Width for buttons
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: AppColors.paleYellow,
+                      foregroundColor: Colors.black,
+                      textStyle: const TextStyle(fontSize: 20),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        side: BorderSide(color: AppColors.yellow, width: 1.0),
+                      ),
+                    ),
+                    onPressed: () => Navigator.pushNamed(context, '/livePlot'),
+                    child: Text(deviceName),
+                  ),
+                ),
+              ),
+            const SizedBox(height: 40),  // Space before the refresh button
+            ElevatedButton(
+              onPressed: () {},  // Placeholder function for refresh
+              child: const Text('Refresh Devices'),
+              style: ElevatedButton.styleFrom(
+                textStyle: const TextStyle(fontSize: 20),
+              ),
+            ),
+          ],
         ),
-        ElevatedButton(onPressed: scanForDevices, child: const Text('Refresh Devices')),
-      ],
+      ),
     );
   }
 }
---
+++ lib/utils/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color darkBlue= Color(0xFF0C345A); 
+  static const Color accentColor = Color(0xFFFFAB40); 
+  static const Color backgroundGrey = Color(0xFFF4F4F4); 
+  static const Color paleYellow = Color(0xFFF7E08D);
+  static const Color yellow = Color(0xFFF0C62D);
+  static const Color palePink= Color(0xFFFFC2D6);
+  static const Color darkPink= Color(0xFFF50052);
+}
