b1668d70becc030e94f157266755c803a1627d11
GOH8910 <GOH8910@GMAIL.COM>
2025-01-13 20:34:12 +0000

    generate random data for the graphs

--- lib/screens/live_plot_page.dart
+++ lib/screens/live_plot_page.dart
@@ -1,4 +1,5 @@
 // live_plot_page.dart
+import 'dart:math';
 import 'package:baby_monitoring_app/utils/colors.dart';
 import 'package:flutter/material.dart';
 import '../widgets/data_model.dart';
@@ -7,79 +8,112 @@ import '../widgets/graph.dart';
 class LivePlotPage extends StatelessWidget {
   const LivePlotPage({super.key});
 
-
   @override
   Widget build(BuildContext context) {
+    final appState = Provider.of<AppStateProvider>(context);
+    final List<String> channelNames = appState.channelNames;
+    final Map<String, List<ChartData>> channelData =
+        appState.channelData; // auto gen data for now
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(60.0), 
+        preferredSize: Size.fromHeight(60.0),
         child: Container(
           decoration: BoxDecoration(
             boxShadow: [
               BoxShadow(
-                color: Colors.black.withOpacity(0.5), 
-                spreadRadius: 1, 
-                blurRadius: 10, 
-                offset: Offset(0, 3), 
+                color: Colors.black.withOpacity(0.5),
+                spreadRadius: 1,
+                blurRadius: 10,
+                offset: Offset(0, 3),
               ),
             ],
           ),
           child: AppBar(
             title: const Text(
               'Real-time Blood Parameter Monitoring',
-              style: TextStyle(color: Colors.white, fontSize: 30.0, fontWeight: FontWeight.bold),
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold),
             ),
-            backgroundColor: AppColors.darkBlue, 
+            backgroundColor: AppColors.darkBlue,
             centerTitle: true,
             actions: <Widget>[
               TextButton(
                 onPressed: () {
-                  // Idk 
+                  // Static Plot goes here
                 },
                 style: TextButton.styleFrom(
-                  backgroundColor: AppColors.palePink, 
+                  backgroundColor: AppColors.palePink,
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0), 
-                    side: BorderSide(color: AppColors.darkPink, width: 1.5), 
+                    borderRadius: BorderRadius.circular(15.0),
+                    side: BorderSide(color: AppColors.darkPink, width: 1.5),
                   ),
                 ),
-                child: const Text('Make plots static', style: TextStyle(color: Colors.black, fontSize: 15.0)),
+                child: const Text('Make plots static',
+                    style: TextStyle(color: Colors.black, fontSize: 15.0)),
               ),
               IconButton(
-                icon: const Icon(Icons.arrow_drop_down, color: Colors.white, size: 50),
+                icon: const Icon(Icons.arrow_drop_down,
+                    color: Colors.white, size: 50),
                 onPressed: () => _showPopupMenu(context),
               ),
             ],
           ),
         ),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: GraphWidget(number: 1, paramName: 'glucose', data: glucoseData, lineColor: Colors.green, plotType: 'l'),
-          ),
-          Expanded(
-            child: GraphWidget(number: 2, paramName: 'lactate', data: lactateData, lineColor: Colors.red, plotType: 'l'),
-          ),
-        ],
-      ),
+      body: channelNames.isNotEmpty
+          ? Column(
+              children: channelNames.map((currentChannel) {
+                final int index = channelNames.indexOf(currentChannel);
+                final List<ChartData>? currentData =
+                    channelData[currentChannel];
+
+                if (currentData == null) {
+                  return Center(
+                    child: Text(
+                      'No data available for $currentChannel',
+                      style: const TextStyle(fontSize: 16, color: Colors.grey),
+                    ),
+                  );
+                }
+
+                return Expanded(
+                  child: GraphWidget(
+                    number: index + 1,
+                    paramName: currentChannel,
+                    data: generateRandomData(10),
+                    lineColor: _getChannelColor(currentChannel),
+                    plotType: 'l',
+                  ),
+                );
+              }).toList(),
+            )
+          : const Center(
+              child: Text(
+                'No channels available. Please add a channel.',
+                style: TextStyle(fontSize: 18, color: Colors.grey),
+              ),
+            ),
     );
   }
-}
- void _showPopupMenu(BuildContext context) async {
+
+  void _showPopupMenu(BuildContext context) async {
     await showMenu<String>(
       context: context,
-      position: RelativeRect.fromLTRB(200.0, 80.0, 0.0, 0.0), //IconButton position
+      position:
+          RelativeRect.fromLTRB(200.0, 80.0, 0.0, 0.0), //IconButton position
       items: [
         PopupMenuItem<String>(
           value: 'past_recordings',
           onTap: () {
-            Navigator.pushNamed(context, '/pastRecordings'); // Navigation to past recordings
+            Navigator.pushNamed(
+                context, '/pastRecordings'); // Navigation to past recordings
           },
-          child: 
-          const Text('Go to past recordings',
-          textAlign: TextAlign.center, 
-          style: TextStyle(fontSize: 18), 
+          child: const Text(
+            'Go to past recordings',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 18),
           ),
         ),
       ],
@@ -87,15 +121,37 @@ class LivePlotPage extends StatelessWidget {
     );
   }
 
+  Color _getChannelColor(String paramName) {
+    switch (paramName) {
+      case 'glucose':
+        return Colors.green;
+      case 'lactate':
+        return Colors.red;
+      default:
+        return _generateRandomColor();
+    }
+  }
 
-final List<ChartData> glucoseData = [
-  ChartData(DateTime.now(), 4),
-  ChartData(DateTime.now().add(const Duration(minutes: 1)), 5),
-  ChartData(DateTime.now().add(const Duration(minutes: 2)), 4),
-  ChartData(DateTime.now().add(const Duration(minutes: 3)), 5),
-];
+  Color _generateRandomColor() {
+    final Random random = Random();
+    return Color.fromARGB(
+      255, // Fully opaque
+      random.nextInt(256), // Random red value (0-255)
+      random.nextInt(256), // Random green value (0-255)
+      random.nextInt(256), // Random blue value (0-255)
+    );
+  }
+}
 
-final List<ChartData> lactateData = [
-  ChartData(DateTime.now(), 1),
-  ChartData(DateTime.now().add(const Duration(minutes: 1)), 1),
-];
+List<ChartData> generateRandomData(int count) {
+  final Random random = Random();
+  final DateTime startTime = DateTime.now();
+
+  return List<ChartData>.generate(count, (index) {
+    final DateTime time =
+        startTime.add(Duration(seconds: index * 5)); // 5-second intervals
+    final double value = 50.0 +
+        random.nextDouble() * 50.0; // Random value between 50.0 and 100.0
+    return ChartData(time, value);
+  });
+}
