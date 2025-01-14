295378c2098837ef486e6fb75484c6addd4bd1c9
mael scott de martinville <ms4822@ic.ac.uk>
2025-01-14 00:24:44 +0000

    Update bluetooth_page.dart
    back button and navigation changes

--- lib/screens/bluetooth_page.dart
+++ lib/screens/bluetooth_page.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:baby_monitoring_app/utils/colors.dart'; // Ensure colors.dart is correctly imported
+import 'package:baby_monitoring_app/screens/first_screen.dart'; // Import for navigation
 
 class BluetoothDevicePage extends StatelessWidget {
   const BluetoothDevicePage({super.key});
@@ -10,15 +11,15 @@ class BluetoothDevicePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(80.0), //AppBar height
+        preferredSize: const Size.fromHeight(80.0), // AppBar height
         child: Container(
           decoration: BoxDecoration(
             boxShadow: [
               BoxShadow(
-                color: Colors.black.withOpacity(0.5), 
-                spreadRadius: 1, 
-                blurRadius: 10, 
-                offset: Offset(0, 3),
+                color: Colors.black.withOpacity(0.5),
+                spreadRadius: 1,
+                blurRadius: 10,
+                offset: const Offset(0, 3),
               ),
             ],
           ),
@@ -27,11 +28,17 @@ class BluetoothDevicePage extends StatelessWidget {
               'Bluetooth Device Selection',
               style: TextStyle(color: Colors.white, fontSize: 35.0, fontWeight: FontWeight.bold),
             ),
-            backgroundColor: AppColors.darkBlue, 
+            backgroundColor: AppColors.darkBlue,
             centerTitle: true,
+            leading: IconButton( // Added back button
+              icon: const Icon(Icons.arrow_back, color: Colors.white),
+              onPressed: () {
+                Navigator.pop(context); // Navigates back to the previous page
+              },
+            ),
             actions: <Widget>[
               IconButton(
-                icon: const Icon(Icons.arrow_drop_down, color: Colors.white, size: 50), 
+                icon: const Icon(Icons.arrow_drop_down, color: Colors.white, size: 50),
                 onPressed: () => _showPopupMenu(context),
               ),
             ],
@@ -45,17 +52,17 @@ class BluetoothDevicePage extends StatelessWidget {
   void _showPopupMenu(BuildContext context) async {
     await showMenu<String>(
       context: context,
-      position: RelativeRect.fromLTRB(200.0, 80.0, 0.0, 0.0), //IconButton position
+      position: const RelativeRect.fromLTRB(200.0, 80.0, 0.0, 0.0),
       items: [
         PopupMenuItem<String>(
           value: 'past_recordings',
           onTap: () {
-            Navigator.pushNamed(context, '/pastRecordings'); // Navigation to past recordings
+            Navigator.pushNamed(context, '/pastRecordings'); 
           },
-          child: 
-          const Text('Go to past recordings',
-          textAlign: TextAlign.center, 
-          style: TextStyle(fontSize: 18), 
+          child: const Text(
+            'Go to past recordings',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 18),
           ),
         ),
       ],
@@ -77,10 +84,10 @@ class _BluetoothDeviceListState extends State<BluetoothDeviceList> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 20), 
+      padding: const EdgeInsets.only(top: 20),
       child: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start, 
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             const Padding(
               padding: EdgeInsets.all(30.0),
@@ -93,7 +100,7 @@ class _BluetoothDeviceListState extends State<BluetoothDeviceList> {
               Padding(
                 padding: const EdgeInsets.symmetric(vertical: 8.0),
                 child: SizedBox(
-                  width: 350,  // Width for buttons
+                  width: 350, 
                   child: ElevatedButton(
                     style: ElevatedButton.styleFrom(
                       backgroundColor: AppColors.paleYellow,
@@ -109,13 +116,13 @@ class _BluetoothDeviceListState extends State<BluetoothDeviceList> {
                   ),
                 ),
               ),
-            const SizedBox(height: 40),  // Space before the refresh button
+            const SizedBox(height: 40),
             ElevatedButton(
-              onPressed: () {},  // Placeholder function for refresh
-              child: const Text('Refresh Devices'),
+              onPressed: () {},  
               style: ElevatedButton.styleFrom(
                 textStyle: const TextStyle(fontSize: 20),
               ),
+              child: const Text('Refresh Devices'),
             ),
           ],
         ),
